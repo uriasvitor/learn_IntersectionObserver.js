bbdc180b01b2d22dcf023d76f74db62b050d698f
uriasvitor <uriasvitor@gmail.com>
2022-09-13 00:44:36 -0300

    icon

--- js/black_theme.js
+++ js/black_theme.js
@@ -1,16 +1,20 @@
 var isValid = true;
 
-dark_theme = document.querySelector('.letter')
+switchBtn = document.querySelector('.letter')
 
 const darkAnimation = ()=>{
     let getTheme = document.querySelector("html");
+    let switchState = document.querySelector('.letter')
 
     if(isValid === true){
         document.querySelector("html").setAttribute("data-theme",'light')
 
         if(getTheme.getAttribute('data-theme') === 'light'){
             localStorage.setItem('light', true)
-            localStorage.removeItem('dark', true)  
+            localStorage.removeItem('dark', true)
+
+            switchState.classList.add("light")
+            switchState.classList.remove("dark")
         }
     }
 
@@ -21,10 +25,13 @@ const darkAnimation = ()=>{
         if(getTheme.getAttribute('data-theme') === 'dark'){
             localStorage.setItem('dark', true)
             localStorage.removeItem('light', true)   
+
+            switchState.classList.add("dark")
+            switchState.classList.remove("light")
         }
     }
         isValid = !isValid;
     
 }
 
-dark_theme.addEventListener('click',darkAnimation)
+switchBtn.addEventListener('click',darkAnimation)
