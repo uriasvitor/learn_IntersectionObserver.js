a1114598e55fc817aed5b6ffc97b691779042f1c
uriasvitor <uriasvitor@gmail.com>
2022-09-13 00:13:30 -0300

    logo

--- js/animation.js
+++ js/animation.js
@@ -1,8 +1,77 @@
-let getLetter = document.querySelector(".logo")
+const getLetter = document.querySelector(".logo");
+const state = document.querySelector(".letter");
+const animaDuration = 1000;
 
 const setLetter = (e)=>{
     e.preventDefault()
-    document.querySelector(".logo").setAttribute('data-animate')
+
+    document.querySelector(".letter").setAttribute("data-animate",'running')
+    document.querySelector(".logo_img").setAttribute("data-animate",'running')
+
+    setTimeout(() => {
+        document.querySelector(".letter").removeAttribute("data-animate",'running');
+        document.querySelector(".logo_img").removeAttribute("data-animate",'running');
+    }, animaDuration);
+
+    if(state.getAttribute('data-animate') == 'running'){
+        getLetter.removeEventListener("mouseover",setLetter)
+    }
+
+    document.querySelector(".letter").animate(
+        [
+            {   
+                transform:  'rotate(0deg)',
+                height: '28px'
+                
+            },
+
+            { 
+                transform: 'rotate(90deg)',
+                opacity: 0.2,
+                borderTop: '2px solid black',
+                height: '30px'
+                
+            },
+
+            {   transform: 'rotate(90deg)',
+                borderRadius:'26px',
+                borderTop: '2px solid black',
+                height: '40px'
+            },
+
+
+        ], 
+        
+        {
+          fill: 'forwards',
+          duration: animaDuration
+
+        });
+
+        document.querySelector(".logo_img").animate(
+            [
+                {   
+                    margin:'0 0' 
+
+                },
+    
+                {   
+                    margin:'0 10px'
+                },
+
+                {   
+                    margin:'0 15px'
+                }
+    
+            ], 
+            
+            {
+              fill: 'forwards',
+              duration: 300
+    
+            });
+
+
 }
 
 
